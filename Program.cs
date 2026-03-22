3e00094ad3f6d2ed65bffd994eeee284e386b371
Vyacheslav <relaxerrkis@gmail.com>
2026-03-22 18:14:18 +0400

    Basic structure

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем поддержку Razor Pages
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles(); // Для CSS/JS
+app.UseRouting();
+
+app.MapRazorPages(); // Маршрутизация страниц
 
 app.Run();
