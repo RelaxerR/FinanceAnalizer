7afc03b8dc9fb1097c3947110b5362d4a36ca72b
Vyacheslav Kiselev <67540331+RelaxerR@users.noreply.github.com>
2026-03-22 18:18:06 +0400

    Added logger service (#1)

--- Program.cs
+++ Program.cs
@@ -1,13 +1,43 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем поддержку Razor Pages
+// 1. Настройка Serilog
+const string logTemplate = "[{Level:w4}] [{Timestamp:yyyy-MM-dd HH:mm:ss}] [{SourceContext}]: {Message:lj}{NewLine}{Exception}";
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    // Настройка консоли
+    .WriteTo.Console(outputTemplate: logTemplate)
+    // Настройка файла
+    .WriteTo.File("logs/log-.txt", 
+        rollingInterval: RollingInterval.Day, 
+        outputTemplate: logTemplate)
+    .CreateLogger();
+
+// Указываем .NET использовать Serilog
+builder.Host.UseSerilog();
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-app.UseStaticFiles(); // Для CSS/JS
+// Логируем первый запуск
+Log.Information("Приложение запущено и готово к работе");
+
+app.UseStaticFiles();
 app.UseRouting();
+app.MapRazorPages();
 
-app.MapRazorPages(); // Маршрутизация страниц
-
-app.Run();
+try 
+{
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Приложение аварийно завершилось");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
